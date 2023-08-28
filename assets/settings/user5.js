641059ba14aa44e1452ccbb9fd7563d892cb7eb0
unknown <sutulaa672@gmail.com>
2023-08-28 09:38:28 +0500

    add 2 users

---
+++ assets/settings/user5.js
@@ -0,0 +1,3 @@
+// Аккаунт № 5
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDgxLCJleHAiOjE2OTM1Nzc1NzQsImlhdCI6MTY5Mjk3Mjc3NH0.dlGLNJN_cbOZTQnVE65P6hpro4AstBG8Jov4L8AZYwI";
+const ukey = "c6b580d6-b4c6-44cc-96be-709dd21094fa";
---
+++ assets/settings/user6.js
@@ -0,0 +1,3 @@
+// Аккаунт № 6
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ODQ5LCJleHAiOjE2OTM4MDIxNDksImlhdCI6MTY5MzE5NzM0OX0.vdRFJJ9xk9dEk3VZuyJJ5DofWSDk_qExTZdVqA9JU7I";
+const ukey = "f73dbdc1-5e43-459d-a7f9-57e2f421e5b6";
