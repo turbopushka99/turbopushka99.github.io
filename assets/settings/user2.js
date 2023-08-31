d327995f386c90926212bc504949f3348fd6d05a
ArthurMukashev <arthur.mukasev@gmail.com>
2023-08-31 23:59:21 +0500

    fixed token user2

--- assets/settings/user2.js
+++ assets/settings/user2.js
@@ -1,3 +1,3 @@
 // аккаунт № 2
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTgwLCJleHAiOjE2OTM1MDc1OTMsImlhdCI6MTY5MjkwMjc5M30.shd8eYxRc5czn2WprlBKFhJrpArGfhgyW53_3kEG3kw";
-const ukey = "1ee71952-3be7-4db1-8326-a67dc3984320";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTgwLCJleHAiOjE2OTQxMTMwODAsImlhdCI6MTY5MzUwODI4MH0.QLf6Z1PkDEDUTaKhE9eysDiju-2eiYxDH8yJGtB5raU";
+const ukey = "35e142a2-bdfc-4473-adec-1a50086a01bf";
