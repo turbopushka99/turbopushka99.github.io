0f3bbff6ed62b0480e975483850e222816a1af94
ArthurMukashev <arthur.mukasev@gmail.com>
2023-08-31 16:50:16 +0500

    new user

---
+++ assets/settings/user7.js
@@ -0,0 +1,3 @@
+// аккаунт № 7
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAzLCJleHAiOjE2OTQwODcyNTIsImlhdCI6MTY5MzQ4MjQ1Mn0.bYgYRqhK-yWZa-AY8rIM8RqPLuECQ-YoNVp26z9HxUM";
+const ukey = "35e142a2-bdfc-4473-adec-1a50086a01bf";
