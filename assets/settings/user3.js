16b6a4457ec67d61104f89e5873704d178acdc3b
ArthurMukashev <arthur.mukasev@gmail.com>
2023-09-03 17:03:17 +0500

    fix user3 token

--- assets/settings/user3.js
+++ assets/settings/user3.js
@@ -1,3 +1,3 @@
 // аккаунт № 3
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTQ0LCJleHAiOjE2OTM3MzcwODQsImlhdCI6MTY5MzEzMjI4NH0.Pq7Jeh2FiWQ4-8O2q1tbI_xvL25wROIJAewQrJ3BCJU";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTQ0LCJleHAiOjE2OTQzNDczMzQsImlhdCI6MTY5Mzc0MjUzNH0.TVXDggiqoqmM1l7FOuzx9pNZioHapZLDFvbQHWhJ5-g\n";
 const ukey = "35e142a2-bdfc-4473-adec-1a50086a01bf";
