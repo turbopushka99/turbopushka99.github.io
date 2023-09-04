68549dac31bf5cf990a5eeecd54d13ecc06b3999
ArthurMukashev <arthur.mukasev@gmail.com>
2023-09-04 09:31:50 +0500

    fix user4 token

--- assets/settings/user4.js
+++ assets/settings/user4.js
@@ -1,3 +1,3 @@
 // аккаунт № 4
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Mjg4LCJleHAiOjE2OTM3NzExODQsImlhdCI6MTY5MzE2NjM4NH0.TEqAQ-QSoZ9Yug-V0SIJmIoIxpo-1lpVgDPI1baqUcc";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Mjg4LCJleHAiOjE2OTQ0MDY2NzgsImlhdCI6MTY5MzgwMTg3OH0.yr2xCuAPBunsY-J7i9l9bp6v5GJIg4FBu8yXn3EDRXc";
 const ukey = "35e142a2-bdfc-4473-adec-1a50086a01bf";
