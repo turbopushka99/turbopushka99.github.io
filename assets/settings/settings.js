277f835bc8b3e4bd494fc48a8af92747a02f666f
ArthurMukashev <arthur.mukasev@gmail.com>
2023-09-04 21:09:17 +0500

    fix index token

--- assets/settings/settings.js
+++ assets/settings/settings.js
@@ -1,5 +1,5 @@
 // аккаунт № 1
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTE5LCJleHAiOjE2OTM4NDIyMjYsImlhdCI6MTY5MzIzNzQyNn0.jTSDbExsQSdViUmjC8GknRZDGqvXInVOUY4kGbjxQJA";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTE5LCJleHAiOjE2OTQ0NDc2NTksImlhdCI6MTY5Mzg0Mjg1OX0.Kw2sRBWCkGHrTz1xDtXdCg86adXPDCsD8Nq9RYRdaOI";
 const ukey = "398361b3-0f5f-4c75-8986-af9ad40704a6";
 
 $('#turboAccelerate').attr('data-bs-toggle', '');
