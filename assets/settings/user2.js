a5a668ecbe4db6f83aac4a21e0c3eaa198343337
ArthurMukashev <arthur.mukasev@gmail.com>
2023-09-08 00:11:32 +0500

    fix user2

--- assets/settings/user2.js
+++ assets/settings/user2.js
@@ -1,3 +1,3 @@
 // аккаунт № 2
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTgwLCJleHAiOjE2OTQxMTMzMjcsImlhdCI6MTY5MzUwODUyN30.QJQAUrZ3ICzDSm83DxD_nOMcGdLtnsOkPoAc3NKKguk";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTgwLCJleHAiOjE2OTQ3MTg2NjIsImlhdCI6MTY5NDExMzg2Mn0.pBKkZ8JKCduiWUJyMay0olgTpnxUNRxwWpU00zx4xTk";
 const ukey = "35e142a2-bdfc-4473-adec-1a50086a01bf";
